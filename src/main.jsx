4519ac198bee215f5e34c8492b7124fb39d9e4f1
Daniel <danilacasito8@gmail.com>
2025-04-22 10:16:09 +0200

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.jsx';
 import { createBrowserRouter, RouterProvider } from "react-router";
+import User from "./User.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />
+    },
+    {
+        path: '/users/:username',
+        loader: async ({params}) => {
+            return {
+                username: params.username
+            }
+        },
+        Component: User
     }
 ])
 createRoot(document.getElementById('root')).render(
