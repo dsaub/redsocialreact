f09a8499a6e7d4b1e32b987cead7048bb8904fe5
Daniel <danilacasito8@gmail.com>
2025-04-22 16:18:30 +0200

    Some things that are going to get merged

---
+++ src/User.jsx
@@ -0,0 +1,12 @@
+import { useLoaderData } from "react-router";
+
+function User() {
+    let data = useLoaderData();
+    return (
+        <div id="app">
+            <main>
+                
+            </main>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import User from './User.jsx';
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />
+    },
+    {
+        path: '/user/:username',
+        loader: async ({params}) => {
+            return {
+                name: params.username
+            };
+        },
+        Component: User
     }
 ])
 createRoot(document.getElementById('root')).render(
