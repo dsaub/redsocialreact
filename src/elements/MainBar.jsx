3e0d7660b44eba001504f50884abfa797d54b53e
Daniel <danilacasito8@gmail.com>
2025-04-23 14:29:00 +0200

    Add all new Login form (WIP)

--- src/elements/MainBar.jsx
+++ src/elements/MainBar.jsx
@@ -1,13 +1,17 @@
 import "./MainBar.css";
-
+import Login from './Login'
 function MainBar() {
 
     return (
+        <>
         <header>
             <div id="title">
                 Red Social
             </div>
         </header>
+
+        <Login />
+        </>
     );
 }
 
