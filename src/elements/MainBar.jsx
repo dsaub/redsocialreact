d277bc0cddfdccf3093ce255e9558015107a417c
Daniel <danilacasito8@gmail.com>
2025-04-24 12:19:17 +0200

    Improve UI

--- src/elements/MainBar.jsx
+++ src/elements/MainBar.jsx
@@ -1,16 +1,24 @@
 import "./MainBar.css";
+import { useState } from 'react';
 import Login from './Login'
+import LoginBtn from './LoginBtn';
 function MainBar() {
-
+    
+    const [loginShown, setLoginShown] = useState(false);
+    const [loggedIn, setLoggedIn] = useState(false);
+    function loginalt() {
+        setLoginShown(!loginShown);
+    }
     return (
         <>
         <header>
             <div id="title">
                 Red Social
             </div>
+            {loggedIn ? '' : <LoginBtn pushed={ loginalt } />}
         </header>
-
-        <Login />
+        {loginShown ? <Login /> : ''}
+        
         </>
     );
 }
